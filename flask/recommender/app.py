5b656ff01772ad33e4b7d35f32b2059ca49cdede
drd4n <patorn.5@mail.kmutt.ac.th>
2021-09-25 13:16:11 +0700

    ADD get all posts

--- flask/recommender/app.py
+++ flask/recommender/app.py
@@ -10,7 +10,7 @@ import db
 
 #Route
 @app.route('/recommendedPosts', methods=["GET"])
-def seeAllPosts():
+def recommendedPosts():
     # try:
         user_id = request.args.get('user_id')
         interactions = tuple(db.db.interactions.aggregate([
@@ -48,6 +48,28 @@ def seeAllPosts():
         # response=json.dumps({"test":"test"}),
         status=200,
         mimetype="application/json")
+
+@app.route('/posts', methods=["GET"])
+def seeAllPosts():
+    try:
+        data = list(db.db.posts.find())
+        for post in data:
+            post["_id"] = str(post["_id"])
+            post["picture_link"] = list(post["picture_link"])
+            post["tags"] = list(post["tags"])
+            for user in post["owner"]:
+                user["_id"] = str(user["_id"])
+        return Response(
+            response=json.dumps(data),
+            status=200,
+            mimetype="application/json")
+    except Exception as ex:
+        print(ex)
+        return Response(
+            response=json.dumps({"errors":"cannot get posts"}),
+            status=500,
+            mimetype="application/json")
+    return "flask mongodb atlas!"
 
 #export
 if __name__ == '__main__':
