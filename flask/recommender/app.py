a37ff4fb04ebadc9c72a10cc868c3236540a1b52
poonypk_ <poon.roon3@mail.kmutt.ac.th>
2021-11-27 15:47:05 -0800

    [UPDATE] import

--- flask/recommender/app.py
+++ flask/recommender/app.py
@@ -1,13 +1,13 @@
 from array import array
 from re import match
-from flask import Flask, request, jsonify
 import flask
 from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
 from flask.wrappers import Response
 import json
 from werkzeug.wrappers import response
-app = Flask(__name__)
+from bson.objectid import ObjectId
+from bson.json_util import dumps
 import db
 from recommendation import lightfmReccomend
 
